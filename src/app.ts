47ef06960f5571b0224dbd4e2c4c2c84477dad3d
rasel khan <tunnerbd2016@gmail.com>
2024-06-24 03:13:19 +0600

    fix some problem

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import { StudentRoutes } from './app/modules/student/student.route';
-=======
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -12,7 +7,6 @@ import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
->>>>>>> part-1
 
 const app: Application = express();
 
@@ -21,29 +15,19 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-<<<<<<< HEAD
-app.use('/api/v1/students', StudentRoutes);
-
-const getAController = (req: Request, res: Response) => {
-=======
 app.use('/api/v1', router);
 
 const test = (req: Request, res: Response) => {
 
->>>>>>> part-1
   const a = 10;
   res.send(a);
 };
 
-<<<<<<< HEAD
-app.get('/', getAController);
-=======
 app.get('/', test);
 
 app.use(globalErrorHandler);
 
 //Not Found
 app.use(notFound);
->>>>>>> part-1
 
 export default app;
